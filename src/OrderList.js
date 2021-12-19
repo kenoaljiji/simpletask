c571d36f381bddc8f6a71d15b2bd8628ac67dc6d
kenoaljiji <studioaljiji@gmail.com>
2021-12-19 16:42:47 +0100

    finishing a task

---
+++ src/OrderList.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const OrderList = () => {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+  const [changedOrder, setChangedOrder] = useState(false);
+
+  const addList = (e) => {
+    if (e.key === "Enter" && text !== "") {
+      setList([...list, text]);
+      setText("");
+    }
+  };
+
+  const sortListHandeler = () => {
+    setChangedOrder(!changedOrder);
+  };
+
+  if (changedOrder) {
+    list.sort((a, b) => {
+      if (a > b) {
+        return -1;
+      }
+      if (b > a) {
+        return 1;
+      }
+      return 0;
+    });
+  } else {
+    list.sort();
+  }
+
+  return (
+    <div>
+      <>
+        <p>Add to wishList</p>
+        <input
+          type="text"
+          name="inputList"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyPress={(e) => addList(e)}
+        />
+        <button className="sort-direction" onClick={() => sortListHandeler()}>
+          {changedOrder ? "Up" : "Down"}
+        </button>
+        <button className="clear-list" onClick={() => setList([])}>
+          delete
+        </button>
+        <ul className="item-list">
+          {list.length > 0 ? list.map((item) => <li>{item}</li>) : null}
+        </ul>
+      </>
+    </div>
+  );
+};
+
+export default OrderList;
